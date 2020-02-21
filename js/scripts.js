e2247e4d314dcbab3b27285bb0db9e5df19e887e
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-02-21 21:11:42 +0300

    started on a new index file and moved images into one folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-  
+
+$(document).ready(function(){
+  $("#design").click(funct5ion(){
+    $("#designp").toggle();
+  });
+};
+
+$(document).ready(function(){
+  $("#design").click(function(){
+    $("#designp").toggle();
+  });
+};
+
+$(document).ready(function(){
+  $("#work1").hover(function(){
+    $().css("display", "inline");
+    }, function(){
+    $(this).css(back"background-color", "pink");
+  });
+});

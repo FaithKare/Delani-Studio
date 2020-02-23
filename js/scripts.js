a387a50cfcf25f915796881054e67c9695799b00
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-02-23 12:42:03 +0300

    created function for hover effect and alert function for input in contact form

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,53 @@ $(document).ready(function() {
     $("#prodp").toggle();
   });
 });
+
+$('img.workdone').hover(function(){
+      $(this).css('opacity', '0.2');
+    },
+    function(){
+      $(this).css('opacity', '1');
+    }
+);
+function validateForm() {
+  var x = document.forms["contactUs"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateForm() {
+  var x = document.forms["contactUs"]["email"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+}
+function validateForm() {
+  var x = document.forms["contactUs"]["subject"].value;
+  if (x == "") {
+    alert("Please write you comment.");
+    return false;
+  }
+}
+function validateForm() {
+
+var x=document.forms["contactUs"]["name"].value;
+if (x==null || x=="")
+  {
+  alert("Name must be filled out");
+  return false;
+  }
+
+var y=document.forms["contactUs"]["email"].value;
+if (y==null || y=="") {
+  alert("Email must be filled out");
+  return false;
+  }
+
+  var z=document.forms["contactUs"]["subject"].value;
+  if (z==null || z=="") {
+    alert("Please type your message");
+    return false;
+    }
+}
